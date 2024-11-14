2bf9cd512f6d4f91eb0a2065a5f153824e6ebe41
Robin Boone <90094863+Robgito@users.noreply.github.com>
2024-11-14 22:32:51 +0100

    Fixed router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { BlogComponent } from './blog/blog.component';
-import { AdminComponent } from './admin/admin.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'blog', component: BlogComponent },
-  { path: 'admin', component: AdminComponent },
 ];
